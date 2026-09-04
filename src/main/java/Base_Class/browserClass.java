6b4e532561b8e439dfb6fe6b236a6b5befa73fbe
sanjana <sanjana.m@altius.cc>
2026-09-04 15:06:00 +0530

    TC_01 run

--- src/main/java/Base_Class/browserClass.java
+++ src/main/java/Base_Class/browserClass.java
@@ -27,11 +27,11 @@ public class browserClass {
 		//WebDriverManager.chromedriver().setup();
 
 		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("--headless=new");
-//		options.addArguments("--no-sandbox");
-//		options.addArguments("--disable-dev-shm-usage");
-//		options.addArguments("--disable-gpu");
-//		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--headless=new");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1920,1080");
 		
 		
 
